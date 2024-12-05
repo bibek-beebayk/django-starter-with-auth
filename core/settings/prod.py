2af45a9ca6ca0d4cf9d674381c3ce99f1456082f
Bibek Gautam <beebayk0001@gmail.com>
2024-12-05 06:13:41 +0000

    chore: update cors origins in prod

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -6,7 +6,9 @@ SECRET_KEY = 'django-insecure-mrk3h^2+7cas%6q$(6_gt!tixkx)=-)=m)l)id-c1qbm&r2df_
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["alnoor-be-production.up.railway.app"]
+
+CSRF_TRUSTED_ORIGINS = ["https://alnoor-be-production.up.railway.app"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
