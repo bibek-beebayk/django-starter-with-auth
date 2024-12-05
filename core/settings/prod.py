fef6d50bdd7157b5c1d3a0e34f7ddb772cbfdac1
Bibek Gautam <beebayk0001@gmail.com>
2024-12-05 06:43:41 +0000

    chore: update r2 settings

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -39,3 +39,15 @@ LOGGING = {
         },
     },
 }
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+# Cloudflare R2 settings
+ACCOUNT_ID = "ffae1fbd17549c7fc17547a2777a43b0"
+AWS_ACCESS_KEY_ID = "9f95336e5293f68a42eea75b8cdde291"
+AWS_SECRET_ACCESS_KEY = (
+    "a9efe9365301ebc16d2fb91c3348db42776bc2810ab7b7a85bd302c5d4a49f9a"
+)
+AWS_STORAGE_BUCKET_NAME = "alnoor"
+AWS_S3_ENDPOINT_URL = f"https://{ACCOUNT_ID}.r2.cloudflarestorage.com"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
